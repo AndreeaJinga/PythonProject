5513a864c1b5d68334cf0db3c42c3883e8348031
AndreeaJinga <andreea.jinga@yahoo.com>
2022-12-15 14:07:34 +0200

    Some pretty refactor

--- version1.py
+++ version1.py
@@ -2,55 +2,65 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
-background_color = "thistle2"
+main_window_background_color = "thistle2"
 button_color = "thistle"
 active_button_color = "thistle3"
 frame_background = "thistle4"
+boardgame_background_color = "misty rose"
+
+main_window_width = 1000
+main_window_height = 500
 
 
 def one_player_game(main_window):
-    one_player_frame = Frame(main_window, bg="blue", height=450, width=200, highlightthickness=40,
-                             highlightbackground="blue")
+    one_player_frame = Frame(main_window, bg=boardgame_background_color, height=main_window_height,
+                             width=main_window_width, highlightthickness=20,
+                             highlightbackground=boardgame_background_color)
+    one_player_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
     one_player_frame.tkraise()
+
+    c = Canvas(main_window, bg="blue", height=main_window_height-100, width=main_window_width-100,
+               highlightthickness=20, highlightbackground="blue")
+    c.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
 def two_players_game(main_window):
     messagebox.showinfo("Hello", "It works too!!!!")
 
 
-def init():
-    main_window = Tk()
-    main_window.title("Mancala")
-    main_window.geometry("1000x500")
-
-    start_frame = Frame(main_window, bg=background_color, height=450, width=950)
-    start_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
-
-    # blank_label1 = Label(start_frame, text="", width=100, fg=background_color)
-    # blank_label2 = Label(start_frame, text="", width=100)
-    # blank_label1.grid(row=0, column=0, ipadx=10, ipady=10)
-    # blank_label2.grid(row=0, column=2)
-    title_label = Label(start_frame, text="Welcome to Owale!")
-    title_label.grid(row=1, column=1, ipadx=10, ipady=10)
-    players_label = Label(start_frame, text="How many players will play?")
-    players_label.grid(row=2, column=1, ipadx=10, ipady=10)
-
-    one_player_button = Button(start_frame, text="One player!", fg="black")
-    one_player_button.grid(row=3, column=1, ipadx=10, ipady=10)
-    two_players_button = Button(start_frame, text="Two players!", fg="black")
-    two_players_button.grid(row=4, column=1, ipadx=10, ipady=10)
-
-    return main_window
+# def init():
+#     main_window = Tk()
+#     main_window.title("Mancala")
+#     main_window.geometry("1000x500")
+#
+#     start_frame = Frame(main_window, bg=background_color, height=450, width=950)
+#     start_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+#
+#     # blank_label1 = Label(start_frame, text="", width=100, fg=background_color)
+#     # blank_label2 = Label(start_frame, text="", width=100)
+#     # blank_label1.grid(row=0, column=0, ipadx=10, ipady=10)
+#     # blank_label2.grid(row=0, column=2)
+#     title_label = Label(start_frame, text="Welcome to Owale!")
+#     title_label.grid(row=1, column=1, ipadx=10, ipady=10)
+#     players_label = Label(start_frame, text="How many players will play?")
+#     players_label.grid(row=2, column=1, ipadx=10, ipady=10)
+#
+#     one_player_button = Button(start_frame, text="One player!", fg="black")
+#     one_player_button.grid(row=3, column=1, ipadx=10, ipady=10)
+#     two_players_button = Button(start_frame, text="Two players!", fg="black")
+#     two_players_button.grid(row=4, column=1, ipadx=10, ipady=10)
+#
+#     return main_window
 
 
 def init2():
     main_window = Tk()
     main_window.title("Mancala")
-    main_window.geometry("1000x500")
-    main_window.config(bg=background_color)
+    main_window.geometry(f"{main_window_width}x{main_window_height}")
+    main_window.config(bg=main_window_background_color)
 
-    start_frame = Frame(main_window, bg=frame_background, height=450, width=200, highlightthickness=40,
-                        highlightbackground=frame_background)
+    start_frame = Frame(main_window, bg=frame_background, height=main_window_height-50, width=main_window_width/5,
+                        highlightthickness=40, highlightbackground=frame_background)
     start_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
 
     title_label = Label(start_frame, text="Welcome to Owale!", bg=frame_background, padx=10, pady=2.5,
