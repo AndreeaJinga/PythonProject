46c3638ef971b82956ddeee47f4bc4a3b0743908
AndreeaJinga <andreea.jinga@yahoo.com>
2022-12-18 09:29:25 +0200

    some update, logics starting

--- version1.py
+++ version1.py
@@ -1,78 +1,121 @@
+import time
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
+# colors
 main_window_background_color = "thistle2"
 button_color = "thistle"
 active_button_color = "thistle3"
 frame_background = "thistle4"
 boardgame_background_color = "misty rose"
 
+# dimensions
 main_window_width = 1000
 main_window_height = 500
 
+# global variables
+turn = "player_one"
+holes = []
+
+
+# TODO FOR TODAY: set-up pentru logica <-> initializarea jocului <-> apel func dupa draw_board;
+#  facut functia de move + de spefic ce validari ar fi de facut;
+#  de stabilit cum va arata final
+def valid_move(pressed_button):
+    global turn
+    if 0 <= pressed_button <= 5 and turn == "player_two":
+        return True
+    if 6 <= pressed_button <= 11 and turn == "player_one":
+        return True
+    return False
+
 
 def choose_to_move(position_button):
-    print("i need some logics here" + str(position_button))
+    if not valid_move(position_button):
+        print("wrong move")
+    else:
+        print("ok")
 
 
-def one_player_game(main_window):
+def draw_board(main_window, init_flag=False):
+    global holes
     one_player_frame = Frame(main_window, bg=boardgame_background_color, height=main_window_height,
                              width=main_window_width, highlightthickness=20,
                              highlightbackground=boardgame_background_color)
     one_player_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
     one_player_frame.tkraise()
 
-    canvas = Canvas(main_window, bg="white", height=main_window_height-100, width=main_window_width-100,
+    canvas = Canvas(main_window, bg="white", height=main_window_height - 100, width=main_window_width - 100,
                     highlightthickness=20, highlightbackground="white")
     canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-    # TODO: MAKE IT PRETTY
+    # TODO: MAKE IT PRETTY - rectangles
     first_rectangle = canvas.create_rectangle(70, 80, 880, 220)
     second_rectangle = canvas.create_rectangle(70, 230, 880, 370)
     first_row_of_holes = []
     second_row_of_holes = []
     for i in range(0, 6):
-        first_row_of_holes.append(canvas.create_oval(100+i*130, 100, 200+i*130, 200))
-        second_row_of_holes.append(canvas.create_oval(100+i*130, 250, 200+i*130, 350))
+        first_row_of_holes.append(canvas.create_oval(100 + i * 130, 100, 200 + i * 130, 200))
+        second_row_of_holes.append(canvas.create_oval(100 + i * 130, 250, 200 + i * 130, 350))
     buttons = []
     for i in range(0, 6):
         buttons.append(Button(canvas, text="Choose hole", bg=button_color, activebackground=active_button_color,
                               font=("Arial", 12, "bold"), command=lambda position=i: choose_to_move(position)))
-        buttons[i].place(x=95+130*i, y=40)
+        buttons[i].place(x=95 + 130 * i, y=40)
     for i in range(6, 12):
         buttons.append(Button(canvas, text="Choose hole", bg=button_color, activebackground=active_button_color,
                               font=("Arial", 12, "bold"), command=lambda position=i: choose_to_move(position)))
-        buttons[i].place(x=95+130*(i-6), y=380)
+        buttons[i].place(x=95 + 130 * (i - 6), y=380)
+    for i, hole in enumerate(holes):
+        if 0 <= i <= 5:
+            canvas.create_text(150 + i * 130, 150, text=str(hole), font=("Arial", 25, "bold"))
+            if not init_flag:
+                time.sleep(0.5)
+        else:
+            canvas.create_text(150 + (i-6) * 130, 300, text=str(hole), font=("Arial", 25, "bold"))
+            if not init_flag:
+                time.sleep(0.5)
+    # turn_label = Label(canvas, text="Player One Turn")
+    return buttons, canvas
 
 
+def init_game():
+    global holes
+    for i in range(0, 12):              # 4 4 4 4 4 4 <- player2
+        holes.append(4)                 # 4 4 4 4 4 4 <- player1
+    player_one_score = 0
+    player_two_score = 0
+    return player_one_score, player_two_score
+
+
+# TODO
+def a_player_cannot_move():
+    global holes
+    return False
+
+
+def is_game_over(player_one_score, player_two_score):
+    if player_one_score >= 24:
+        return True
+    if player_two_score >= 24:
+        return True
+    if a_player_cannot_move():
+        return True
+    return False
+
+
+def one_player_game(main_window):
+    global turn, holes
+    player_one_score, player_two_score = init_game()
+    buttons, canvas = draw_board(main_window, init_flag=True)
+
+
+# TODO
 def two_players_game(main_window):
-    messagebox.showinfo("Hello", "It works too!!!!")
-
-
-# def init():
-#     main_window = Tk()
-#     main_window.title("Mancala")
-#     main_window.geometry("1000x500")
-#
-#     start_frame = Frame(main_window, bg=background_color, height=450, width=950)
-#     start_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
-#
-#     # blank_label1 = Label(start_frame, text="", width=100, fg=background_color)
-#     # blank_label2 = Label(start_frame, text="", width=100)
-#     # blank_label1.grid(row=0, column=0, ipadx=10, ipady=10)
-#     # blank_label2.grid(row=0, column=2)
-#     title_label = Label(start_frame, text="Welcome to Owale!")
-#     title_label.grid(row=1, column=1, ipadx=10, ipady=10)
-#     players_label = Label(start_frame, text="How many players will play?")
-#     players_label.grid(row=2, column=1, ipadx=10, ipady=10)
-#
-#     one_player_button = Button(start_frame, text="One player!", fg="black")
-#     one_player_button.grid(row=3, column=1, ipadx=10, ipady=10)
-#     two_players_button = Button(start_frame, text="Two players!", fg="black")
-#     two_players_button.grid(row=4, column=1, ipadx=10, ipady=10)
-#
-#     return main_window
+    global turn
+    player_one_score, player_two_score = init_game()
+    buttons, canvas = draw_board(main_window, init_flag=True)
 
 
 def init2():
