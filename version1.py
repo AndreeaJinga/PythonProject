ec9c7d4d1e2d240ae6d345d428245236c2411d73
AndreeaJinga <andreea.jinga@yahoo.com>
2022-12-18 22:30:57 +0200

    added some logics and some todos :))

--- version1.py
+++ version1.py
@@ -1,8 +1,9 @@
 import time
 from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
+# from tkinter import ttk
+# from tkinter import messagebox
 
+# TODO PALETAR FRUMOS!!!!
 # colors
 main_window_background_color = "thistle2"
 button_color = "thistle"
@@ -16,7 +17,13 @@ main_window_height = 500
 
 # global variables
 turn = "player_one"
+turn_label = Label(None)
 holes = []
+player_one_score = 0
+player_one_score_label = Label(None)
+player_two_score = 0
+player_two_score_label = Label(None)
+warning_unit = 0
 
 
 # TODO FOR TODAY: set-up pentru logica <-> initializarea jocului <-> apel func dupa draw_board;
@@ -31,22 +38,73 @@ def valid_move(pressed_button):
     return False
 
 
-def choose_to_move(position_button):
+def get_next_position_in_weird_circle(current_position):
+    position_to_change = 0
+    if 6 <= current_position <= 10:
+        position_to_change = current_position + 1
+    elif 1 <= current_position <= 5:
+        position_to_change = current_position - 1
+    elif current_position == 0:
+        position_to_change = 6
+    elif current_position == 11:
+        position_to_change = 5
+    return position_to_change
+
+
+def move(starting_position):
+    global holes
+    position_to_change = get_next_position_in_weird_circle(starting_position)
+    for i in range(0, holes[starting_position]):
+        if position_to_change == starting_position:
+            continue
+        holes[position_to_change] += 1
+        position_to_change = get_next_position_in_weird_circle(position_to_change)
+    holes[starting_position] = 0
+    points = calculate_points(position_to_change)#TODO de verif aici ca e pos to change sau -1/+1
+    #TODO: caculate+points
+    #TODO: draw_holes_number in functie
+
+
+def choose_to_move2(position_button):
+    global player_two_score, player_two_score_label, turn, turn_label, warning_unit
     if not valid_move(position_button):
-        print("wrong move")
+        turn_label.config(font=("Arial", 9+warning_unit, "bold"))
+        warning_unit += 1
     else:
-        print("ok")
+        warning_unit = 0
+        turn_label.config(font=("Arial", 9))
+
+        points = move(position_button)
+        player_two_score += points
+        player_two_score_label.config(text=f"Player Two Score: {str(player_two_score)}")
+
+        turn = "player_one"
+        turn_label.config(text="It is player one turn!")
+
+
+def choose_to_move1(position_button):
+    global player_one_score, player_one_score_label, turn, turn_label, warning_unit
+    if not valid_move(position_button):
+        turn_label.config(font=("Arial", 9+warning_unit, "bold"))
+        warning_unit += 1
+    else:
+        warning_unit = 0
+        turn_label.config(font=("Arial", 9))
+        player_one_score += 1
+        player_one_score_label.config(text=f"Player One Score: {str(player_one_score)}")
+        turn = "player_two"
+        turn_label.config(text="It is player two turn!")
 
 
 def draw_board(main_window, init_flag=False):
-    global holes
+    global holes, player_two_score, player_one_score, turn_label, player_one_score_label, player_two_score_label
     one_player_frame = Frame(main_window, bg=boardgame_background_color, height=main_window_height,
                              width=main_window_width, highlightthickness=20,
                              highlightbackground=boardgame_background_color)
     one_player_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
     one_player_frame.tkraise()
 
-    canvas = Canvas(main_window, bg="white", height=main_window_height - 100, width=main_window_width - 100,
+    canvas = Canvas(main_window, bg="white", height=main_window_height - 90, width=main_window_width - 100,
                     highlightthickness=20, highlightbackground="white")
     canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
 
@@ -61,11 +119,13 @@ def draw_board(main_window, init_flag=False):
     buttons = []
     for i in range(0, 6):
         buttons.append(Button(canvas, text="Choose hole", bg=button_color, activebackground=active_button_color,
-                              font=("Arial", 12, "bold"), command=lambda position=i: choose_to_move(position)))
+                              font=("Arial", 12, "bold"),
+                              command=lambda position=i: choose_to_move2(position)))
         buttons[i].place(x=95 + 130 * i, y=40)
     for i in range(6, 12):
         buttons.append(Button(canvas, text="Choose hole", bg=button_color, activebackground=active_button_color,
-                              font=("Arial", 12, "bold"), command=lambda position=i: choose_to_move(position)))
+                              font=("Arial", 12, "bold"),
+                              command=lambda position=i: choose_to_move1(position)))
         buttons[i].place(x=95 + 130 * (i - 6), y=380)
     for i, hole in enumerate(holes):
         if 0 <= i <= 5:
@@ -76,17 +136,25 @@ def draw_board(main_window, init_flag=False):
             canvas.create_text(150 + (i-6) * 130, 300, text=str(hole), font=("Arial", 25, "bold"))
             if not init_flag:
                 time.sleep(0.5)
-    # turn_label = Label(canvas, text="Player One Turn")
+    turn_label = Label(canvas, text="It is player One turn!", bg=boardgame_background_color)
+    turn_label.place(x=10, y=10)
+
+    player_one_score_label = Label(canvas, text=f"Player One Score: {str(player_one_score)}",
+                                   bg=boardgame_background_color, font=("Arial", 10, "bold"))
+    player_one_score_label.place(x=400, y=420)
+
+    player_two_score_label = Label(canvas, text=f"Player Two Score: {str(player_two_score)}",
+                                   bg=boardgame_background_color, font=("Arial", 10, "bold"))
+    player_two_score_label.place(x=400, y=10)
     return buttons, canvas
 
 
 def init_game():
-    global holes
+    global holes, player_one_score, player_two_score
     for i in range(0, 12):              # 4 4 4 4 4 4 <- player2
         holes.append(4)                 # 4 4 4 4 4 4 <- player1
     player_one_score = 0
     player_two_score = 0
-    return player_one_score, player_two_score
 
 
 # TODO
@@ -96,7 +164,8 @@ def a_player_cannot_move():
 
 
 # TODO: not used
-def is_game_over(player_one_score, player_two_score):
+def is_game_over():
+    global player_one_score, player_two_score
     if player_one_score >= 24:
         return True
     if player_two_score >= 24:
@@ -105,17 +174,17 @@ def is_game_over(player_one_score, player_two_score):
         return True
     return False
 
-#de sters comentariul asta
-def one_player_game(main_window):
+
+def one_player_game_init(main_window):
     global turn, holes
-    player_one_score, player_two_score = init_game()
+    init_game()
     buttons, canvas = draw_board(main_window, init_flag=True)
 
 
 # TODO
 def two_players_game(main_window):
     global turn
-    player_one_score, player_two_score = init_game()
+    init_game()
     buttons, canvas = draw_board(main_window, init_flag=True)
 
 
@@ -137,7 +206,7 @@ def init2():
     players_label.grid(row=2, ipadx=10, ipady=10, columnspan=2)
 
     one_player_button = Button(start_frame, text="One player!", bg=button_color, activebackground=active_button_color,
-                               font=("Arial", 12, "bold"), command=lambda: one_player_game(main_window))
+                               font=("Arial", 12, "bold"), command=lambda: one_player_game_init(main_window))
     one_player_button.grid(row=3, column=0, ipadx=10, ipady=5, columnspan=1)
     two_players_button = Button(start_frame, text="Two players!", bg=button_color, activebackground=active_button_color,
                                 font=("Arial", 12, "bold"), command=lambda: two_players_game(main_window))
